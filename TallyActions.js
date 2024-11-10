79505041e5199ab0c0d21798a0f6592fae032b26
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-11-10 16:38:02 +0200

    Added Actions code

---
+++ TallyActions.js
@@ -0,0 +1,7 @@
+// Action creators
+const addAction = () => ({ type: 'ADD' });
+const subtractAction = () => ({ type: 'SUBTRACT' });
+const resetAction = () => ({ type: 'RESET' });
+
+// Export the action creators
+export { addAction, subtractAction, resetAction };
